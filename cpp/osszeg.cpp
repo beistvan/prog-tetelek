f4c5d1392dabb06b29840838a73e5be63445d7d5
beistvan <beistvan@gmail.com>
2016-11-27 07:11:06 +0100

    Create osszeg.cpp

---
+++ cpp/osszeg.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* rand */
+#include <iostream>     /* cout */
+using namespace std;
+
+int a[10];
+int i, sum=0;
+
+int main() {
+    
+    for ( i=0 ; i<10 ; i++ )
+    {
+        a[i] = rand() % 10 + 1;
+        cout << "a[" << i << "]= " << a[i] << "\n";
+        sum += a[i];
+    }
+    cout << "osszeg = " << sum;
+	return 0;
+}
